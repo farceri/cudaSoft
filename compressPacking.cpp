6a243bbcbf1f54e31a9b0f90fcad94fc119a8417
farceri <farceri@uoregon.edu>
2024-04-04 18:13:01 -0400

    added scripts for 2LJ

--- compressPacking.cpp
+++ compressPacking.cpp
@@ -27,10 +27,10 @@ int main(int argc, char **argv) {
   bool gforce = false, alltoall = false, nve = true, nosehoover = false;
   long numParticles = atol(argv[5]), nDim = 2;
   long iteration = 0, maxIterations = 1e05, minStep = 20, numStep = 0;
-  long maxStep = 1e05, step = 0, maxSearchStep = 1500, searchStep = 0;
+  long maxStep = 1e05, step = 0, maxSearchStep = 1500, searchStep = 0, num1;
   long printFreq = int(maxStep / 10), updateCount = 0, saveEnergyFreq = int(printFreq / 10);
   double polydispersity = 0.2, previousPhi, currentPhi, deltaPhi = 6e-02, scaleFactor, isf = 1;
-  double mass = 1, LJcut = 4, cutDistance, cutoff = 0.5, forceTollerance = 1e-08, waveQ, FIREStep = 1e-02, dt = atof(argv[2]);
+  double mass = 1, LJcut = 4, cutDistance, cutoff = 2, forceTollerance = 1e-08, waveQ, FIREStep = 1e-02, dt = atof(argv[2]);
   double ec = 1, ew = 1e02, Tinject = atof(argv[3]), damping, inertiaOverDamping = 10, phi0 = 0.12, phiTh = 0.7;
   double ea = 1, eb = 1, eab = 0.25, timeStep, timeUnit, sigma, maxDelta, lx = atof(argv[4]), gravity = 9.8e-04;
   std::string currentDir, outDir = argv[1], inDir, energyFile;
@@ -96,7 +96,8 @@ int main(int argc, char **argv) {
     cout << "Setting WCA potential" << endl;
   } else if(doublelj == true) {
     sp.setPotentialType(simControlStruct::potentialEnum::doubleLJ);
-    sp.setDoubleLJconstants(LJcut, ea, eab, eb);
+    num1 = numParticles * 0.5;
+    sp.setDoubleLJconstants(LJcut, ea, eab, eb, num1);
   } else {
     cout << "Setting Harmonic potential" << endl;
   }
