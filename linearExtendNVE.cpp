d62fbb7a32b88cf20877d5ae098a02bee4605ba1
farceri <farceri@uoregon.edu>
2024-08-26 14:45:55 -0400

    added saveFinal to extensions

--- linearExtendNVE.cpp
+++ linearExtendNVE.cpp
@@ -23,7 +23,7 @@ using namespace std;
 
 int main(int argc, char **argv) {
   // variables
-  bool readState = true, biaxial = true, reverse = true, equilibrate = false, saveFinal = true;
+  bool readState = true, biaxial = true, reverse = false, equilibrate = false, saveFinal = true;
   bool adjustEkin = false, adjustGlobal = false, adjustTemp = false, save = false, saveCurrent, saveForce = false;
   long step, maxStep = atof(argv[7]), checkPointFreq = int(maxStep / 10), linFreq = int(checkPointFreq / 10);
   long numParticles = atol(argv[8]), nDim = 2, updateCount = 0, direction = 1, initMaxStep = 1e07;
