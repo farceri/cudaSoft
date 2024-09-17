4b04f22d957d1dc7947565946e79740481a72907
farceri <farceri@uoregon.edu>
2024-09-17 11:11:12 -0400

    updated running options

--- linearExtendNVE.cpp
+++ linearExtendNVE.cpp
@@ -23,7 +23,7 @@ using namespace std;
 
 int main(int argc, char **argv) {
   // variables
-  bool readState = true, biaxial = true, reverse = false, equilibrate = false, saveFinal = true;
+  bool readState = true, biaxial = true, reverse = true, equilibrate = false, saveFinal = true;
   bool adjustTemp = false, adjustWall = false, adjustGlobal = false, save = false, saveCurrent, saveForce = false;
   long step, maxStep = atof(argv[7]), checkPointFreq = int(maxStep / 10), linFreq = int(checkPointFreq / 2);
   long numParticles = atol(argv[8]), nDim = 2, updateCount = 0, direction = 1, initMaxStep = 1e07;
