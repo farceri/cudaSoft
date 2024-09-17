7b4da1953a9016331c9ee6bceedad702abfca5a6
farceri <farceri@uoregon.edu>
2024-09-17 11:25:44 -0400

    updated running options

--- runActive2LJ.cpp
+++ runActive2LJ.cpp
@@ -188,7 +188,7 @@ int main(int argc, char **argv) {
         sp.resetUpdateCount();
         if(saveFinal == true) {
           ioSP.saveParticlePacking(outDir);
-          ioSP.saveParticleNeighbors(outDir);
+          //ioSP.saveParticleNeighbors(outDir);
         }
       }
     }
